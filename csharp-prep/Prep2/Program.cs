5eca7d28e4ca910ffb990559182d9dcfe15711de
Nicolas Eiji <nicolas.eiji.morise@gmail.com>
2024-11-08 15:42:54 -0300

    commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What's your grade? ");
+        string grade = Console.ReadLine();
+        int grade_number = int.Parse(grade);
+        string final_grade = "";
+        if (grade_number >= 90){
+            final_grade = "A";
+        }
+        else if (grade_number >= 80){
+            final_grade = "B";
+        }
+        else if (grade_number >= 70){
+            final_grade = "C";
+        }
+        else if (grade_number >= 60){
+            final_grade = "D";
+        }
+        else if (grade_number < 60){
+            final_grade = "F";
+        }
+
+        Console.WriteLine($"Your Grade is {final_grade}");
+
     }
 }

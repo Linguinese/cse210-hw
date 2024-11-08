97a159f0d66c886019227822dbe5db43fc50dc4f
Nicolas Eiji <nicolas.eiji.morise@gmail.com>
2024-11-08 16:02:16 -0300

    commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int number_guess_number;
+
+        Console.Write("What's the magic number? ");
+       string magic_number = Console.ReadLine();
+       int number_magic_number = int.Parse(magic_number); 
+       do{
+            Console.Write("What's your guess? ");
+            string guess_number = Console.ReadLine();
+            number_guess_number = int.Parse(guess_number); 
+            if (number_guess_number > number_magic_number){
+                Console.WriteLine("Lower");
+            }
+            else if (number_guess_number < number_magic_number){
+                Console.WriteLine("Higher");
+            }
+
+       } while (number_guess_number != number_magic_number);
+
+        Console.WriteLine("Correct");
+       
     }
 }

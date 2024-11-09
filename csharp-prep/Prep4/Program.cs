706f8086f4e6625c72540dd612c3afb922a2451c
Nicolas Eiji <nicolas.eiji.morise@gmail.com>
2024-11-09 20:06:30 -0300

    commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Enter a series of numbers (enter 0 to stop):");
+
+        do
+        {
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+
+        
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"Sum: {sum}");
+
+        double average = numbers.Count > 0 ? (double)sum / numbers.Count : 0;
+        Console.WriteLine($"Average: {average}");
+
+        int max = numbers.Count > 0 ? numbers.Max() : 0;
+        Console.WriteLine($"Max: {max}");
     }
 }
